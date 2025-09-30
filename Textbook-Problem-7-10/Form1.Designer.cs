7befd3833bbb79add5fd374528617fcb488ee01f
Zac Lan <lanyk@bu.edu>
2025-09-30 10:29:06 -0400

    pushing 7-10

--- Textbook-Problem-7-10/Form1.Designer.cs
+++ Textbook-Problem-7-10/Form1.Designer.cs
@@ -28,12 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            fruitPic1 = new PictureBox();
+            fruitPic2 = new PictureBox();
+            fruitPic3 = new PictureBox();
+            amountTextBox = new TextBox();
+            amountLabel = new Label();
+            spinBtn = new Button();
+            exitBtn = new Button();
+            ((System.ComponentModel.ISupportInitialize)fruitPic1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)fruitPic2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)fruitPic3).BeginInit();
+            SuspendLayout();
+            // 
+            // fruitPic1
+            // 
+            fruitPic1.Image = Properties.Resources.Apple;
+            fruitPic1.Location = new Point(278, 194);
+            fruitPic1.Name = "fruitPic1";
+            fruitPic1.Size = new Size(126, 130);
+            fruitPic1.TabIndex = 0;
+            fruitPic1.TabStop = false;
+            // 
+            // fruitPic2
+            // 
+            fruitPic2.Image = Properties.Resources.Cherries;
+            fruitPic2.Location = new Point(474, 194);
+            fruitPic2.Name = "fruitPic2";
+            fruitPic2.Size = new Size(144, 130);
+            fruitPic2.TabIndex = 1;
+            fruitPic2.TabStop = false;
+            // 
+            // fruitPic3
+            // 
+            fruitPic3.Image = Properties.Resources.Banana;
+            fruitPic3.Location = new Point(674, 194);
+            fruitPic3.Name = "fruitPic3";
+            fruitPic3.Size = new Size(131, 130);
+            fruitPic3.TabIndex = 2;
+            fruitPic3.TabStop = false;
+            // 
+            // amountTextBox
+            // 
+            amountTextBox.Location = new Point(474, 386);
+            amountTextBox.Name = "amountTextBox";
+            amountTextBox.Size = new Size(200, 39);
+            amountTextBox.TabIndex = 3;
+            // 
+            // amountLabel
+            // 
+            amountLabel.AutoSize = true;
+            amountLabel.Location = new Point(243, 386);
+            amountLabel.Name = "amountLabel";
+            amountLabel.Size = new Size(225, 32);
+            amountLabel.TabIndex = 4;
+            amountLabel.Text = "Amount Inserted: $ ";
+            // 
+            // spinBtn
+            // 
+            spinBtn.Location = new Point(373, 468);
+            spinBtn.Name = "spinBtn";
+            spinBtn.Size = new Size(150, 46);
+            spinBtn.TabIndex = 5;
+            spinBtn.Text = "Spin";
+            spinBtn.UseVisualStyleBackColor = true;
+            spinBtn.Click += spinBtn_Click;
+            // 
+            // exitBtn
+            // 
+            exitBtn.Location = new Point(550, 468);
+            exitBtn.Name = "exitBtn";
+            exitBtn.Size = new Size(150, 46);
+            exitBtn.TabIndex = 6;
+            exitBtn.Text = "Exit";
+            exitBtn.UseVisualStyleBackColor = true;
+            exitBtn.Click += exitBtn_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1089, 640);
+            Controls.Add(exitBtn);
+            Controls.Add(spinBtn);
+            Controls.Add(amountLabel);
+            Controls.Add(amountTextBox);
+            Controls.Add(fruitPic3);
+            Controls.Add(fruitPic2);
+            Controls.Add(fruitPic1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)fruitPic1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)fruitPic2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)fruitPic3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox fruitPic1;
+        private PictureBox fruitPic2;
+        private PictureBox fruitPic3;
+        private TextBox amountTextBox;
+        private Label amountLabel;
+        private Button spinBtn;
+        private Button exitBtn;
     }
 }
--- Textbook-Problem-7-10/Form1.cs
+++ Textbook-Problem-7-10/Form1.cs
@@ -6,5 +6,87 @@ namespace Textbook_Problem_7_10
         {
             InitializeComponent();
         }
+        //Initializing variables
+        Random random = new Random();
+
+        decimal totalAmountEarned;
+        decimal totalAmountSpent;
+        string message;
+
+        //Array of images 
+        Image[] fruitImgs = new Image[]
+        {
+            Properties.Resources.Lemon,
+            Properties.Resources.Apple,
+            Properties.Resources.Lime,
+            Properties.Resources.Strawberry,
+            Properties.Resources.Pear,
+            Properties.Resources.Grapes,
+            Properties.Resources.Banana,
+            Properties.Resources.Orange,
+            Properties.Resources.Watermelon,
+            Properties.Resources.Cherries
+        };
+
+
+
+        private void spinBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal amountSpent = decimal.Parse(amountTextBox.Text);
+                totalAmountSpent += amountSpent;
+
+                fruitPic1.Image = getRandomFruit();
+                fruitPic2.Image = getRandomFruit();
+                fruitPic3.Image = getRandomFruit();
+
+                decimal amountWon = calculateAmountWon(fruitPic1.Image, fruitPic2.Image, fruitPic3.Image, amountSpent);
+                totalAmountEarned += amountWon;
+
+                MessageBox.Show(message);
+
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Invalid value inputted.");
+            }
+        }
+
+        private Image getRandomFruit()
+        {
+            int randomIndex = random.Next(fruitImgs.Length);
+            return fruitImgs[randomIndex];
+        }
+
+        private decimal calculateAmountWon(Image image1, Image image2, Image image3, decimal betAmount)
+        {
+            decimal winnings = 0;
+            if (image1 == image2 && image2 == image3)
+            {
+                winnings = betAmount * 3;
+                message = $"Jackpot! You have won ${winnings}";
+                return winnings;
+            }
+            else if (image1 == image2 || image2 == image3 || image1 == image3)
+            {
+                winnings = betAmount * 2;
+                message = $"Nice. You have won ${winnings}";
+                return winnings;
+            }
+            else
+            {
+                message = $"Sorry! Better luck next time.";
+                return winnings;
+            }
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"You have spent $ {totalAmountSpent}. You have earned $ {totalAmountEarned}.");
+
+            Application.Exit();
+        }
     }
 }
